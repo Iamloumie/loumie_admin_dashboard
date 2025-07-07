b2456906383fbe1bad5c2ef1d44ce92f94b5ffb3
Iamloumie <lawaladedamolaolumide@gmail.com>
2025-07-07 13:19:32 +0100

    edited the page title

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
   <div className="mt-24">
     <p className="dark:text-gray-200 text-gray-700 text-center m-20 italic">
-      © 2025 All rights reserved by LoumieShoppy.com
+      © 2025 All rights reserved by LoumieShoppy.vercel.app
     </p>
   </div>
 );
